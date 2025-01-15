199bb44f3ce717fe329980bd94d0547489086284
Afruzoa <afruzoa1999@gmail.com>
2025-01-15 14:05:52 +0330

    server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import db from './server/table.js';
-import categoriesRoute from './routes/categories.js';
-import roomsRoute from './routes/rooms.js';
+import express from "express";
+import path from "path";
+import db from "./server/table.js";
+import categoriesRoute from "./routes/categories.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 const port = 3000;
@@ -9,9 +10,21 @@ const port = 3000;
 // Middleware
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.use(express.static(path.join(__dirname, "public")));
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, "localhost", () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
 // Routes
-app.use('/api/categories', categoriesRoute);
-app.use('/api/rooms', roomsRoute);
+app.use("/api/categories", categoriesRoute);
+app.use("/api/rooms", roomsRoute);
 
 // Start the server
 app.listen(port, () => {
